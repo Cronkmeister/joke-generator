0e071ea2d06a44844e81109ccdc231fdcf37a243
Jonny Cronk <cronkmeister@Jonnys-MacBook-Pro.local>
2022-05-18 23:21:26 -0700

    font updated

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,16 @@ export default function Home({ pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1.0"
         ></meta>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <main className={styles.main}>
         <div className="robot">
